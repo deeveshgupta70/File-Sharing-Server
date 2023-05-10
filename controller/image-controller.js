c89466768a7e5a654f91c113480a6fb16bd72116
Deevesh <deeveshgupta70@gmail.com>
2023-05-10 15:12:43 +0530

    inital Commit

---
+++ controller/image-controller.js
@@ -0,0 +1,36 @@
+import File from '../models/file.js';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const uploadImage = async (req, res) => {
+    const fileObj = {
+        path: req.file.path,
+        name: req.file.originalname,
+    }
+    
+    try {
+        const responsePath = `http://localhost:${process.env.PORT}`
+        const file = await File.create(fileObj);
+        res.status(200).json({ path: `${responsePath}/${file._id}`});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const getImage = async (req, res) => {
+    try {   
+        const file = await File.findById(req.params.fileId);
+        
+        file.downloadCount++;
+
+        await file.save();
+
+        res.download(file.path, file.name);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ msg: error.message });
+    }
+}
